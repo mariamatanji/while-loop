58de2003db77c55c70e09e30b5c8edf407135d7a
mariamatanji <38054680+mariamatanji@users.noreply.github.com>
2025-11-08 22:02:44 -0800

    Add ChallengeB class to print numbers from 1 to 100

---
+++ ChallengeB.java
@@ -0,0 +1,14 @@
+public class ChallengeB {
+  
+    public static void main(String[] args) {
+      
+        int x =1;
+        
+        while(x <= 100) {
+          
+          x++;
+          
+          System.out.println("Sum is: " + x);
+        }
+    }
+}
